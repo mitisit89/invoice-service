d45312b8d346099808babd1dbc3abd5e13381e2b
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-01-15 13:26:28 +0300

    refactor:models

--- invoice_service/models.py
+++ invoice_service/models.py
@@ -1,17 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, String, Text
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.sql import func
+from sqlmodel import Field, SQLModel
 
 
-Base = declarative_base()
-
-
-class Application(Base):
-    __tablename__ = "applications"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String, nullable=False)
-    description = Column(Text, nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+class Application(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    user_name: str = Field(index=True)
+    description: str
+    created_at: datetime = Field(default=datetime.now())
