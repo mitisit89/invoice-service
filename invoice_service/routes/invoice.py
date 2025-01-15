71370b4482881f5742eaee5a957044ad8615b34d
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-01-15 14:23:35 +0300

    fix:add await

--- invoice_service/routes/invoice.py
+++ invoice_service/routes/invoice.py
@@ -1,29 +1,36 @@
-from fastapi import APIRouter, HTTPException, Query, Depends
-from invoice_service.config import KAFKA_TOPIC, KAFKA_SERVER
+import json
+from typing import Any
+from aiokafka import AIOKafkaProducer
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.sql import select
+
+from invoice_service.config import KAFKA_SERVER, KAFKA_TOPIC
+from invoice_service.config import get_db
 from invoice_service.models import Application
 from invoice_service.schemas import ApplicationCreate, ApplicationResponse
-from invoice_service.config import get_db
-import json
-from aiokafka import AIOKafkaProducer
-from sqlmodel import Session
+from invoice_service.logger import logging as logger
 
 router = APIRouter()
 
 
-async def send_message(msg):
+async def send_message(msg: dict[str, Any]) -> None:
     producer = AIOKafkaProducer(
         bootstrap_servers=KAFKA_SERVER, value_serializer=lambda v: json.dumps(v).encode("utf-8")
     )
     await producer.start()
     try:
+        logger.info(f"Sending message to Kafka: {msg}")
         metadata = await producer.send_and_wait(topic=KAFKA_TOPIC, value=msg)
-        print(f"Message sent to {metadata.topic} partition {metadata.partition} at offset {metadata.offset}")
+        logger.info(f"Message sent to {metadata.topic} partition {metadata.partition} at offset {metadata.offset}")
+    except Exception as e:
+        logger.error(f"Error sending message to Kafka: {e}")
     finally:
         await producer.stop()
 
 
 @router.post("/", response_model=ApplicationResponse)
-async def create_application(application: ApplicationCreate, db: Session = Depends(get_db)):
+async def create_application(application: ApplicationCreate, db: AsyncSession = Depends(get_db)):
     new_application = Application(user_name=application.user_name, description=application.description)
     db.add(new_application)
     await db.commit()
@@ -43,13 +50,10 @@ async def create_application(application: ApplicationCreate, db: Session = Depen
 
 @router.get("/", response_model=list[ApplicationResponse])
 async def list_applications(
-    db: Session = Depends(get_db),
-    user_name: str | None = Query(None),
+    db: AsyncSession = Depends(get_db),
+    user_name: str = Query(str, title="User Name", min_length=1, max_length=100),
     page: int = Query(1, ge=1),
     size: int = Query(10, ge=1, le=100),
 ):
-    query = db.query(Application)
-    if user_name:
-        query = query.filter(Application.user_name == user_name)
-    total = await db.execute(query)
-    return await db.execute(query.offset((page - 1) * size).limit(size))
+    page = await db.execute(select(Application).where(Application.user_name == user_name))
+    return page.scalars().all()
